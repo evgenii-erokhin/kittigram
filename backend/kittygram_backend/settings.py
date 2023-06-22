501231e0db6d6c9fb54f353e79d99dc766e10d28
Evgeniy <evgeniierokhin@proton.me>
2023-06-22 05:00:54 +0000

    Edit settings.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = (os.getenv('DEBUG', 'False') ==  'True')
 
 ALLOWED_HOSTS = ['130.193.43.144','127.0.0.1','localhost', 'http://kittygramprojects.hopto.org/']
 
